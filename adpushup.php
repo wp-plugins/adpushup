13acd9748dea0662d368e68d45e2c8925f247360
AdPushup <AdPushup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-09 12:46:28 +0000

    Released v1.1
    git-svn-id: https://plugins.svn.wordpress.org/adpushup/trunk@1156847 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adpushup.php
+++ adpushup.php
@@ -4,7 +4,7 @@
   Plugin Name: AdPushup
   Plugin URI: http://adpushup.com
   Description: Maximize your AdSense Ad Revenue!
-  Version: 1.0
+  Version: 1.1
   Author: AdPushup
   Author URI: http://www.adpushup.com
   License: GPL2
@@ -17,16 +17,28 @@ defined('ABSPATH') or die("Cheatin' uh?");
 class AdPushup {
 
     public $plugin_dir;
-    public $version = '1.0';
+    public $version = '1.1';
     public $update_url;
     public $platform;
+    public $plugin_basename;
 
     public function __construct() {
 
-        $this->plugin_dir = plugin_dir_path(__FILE__);
-        require_once 'misc.php';
-        require_once 'injector.php';
-        require_once 'settings.php';
+	$this->plugin_dir = plugin_dir_path(__FILE__);
+	$this->plugin_basename = plugin_basename(__FILE__);
+
+	require_once 'misc.php';
+	require_once 'injector.php';
+	require_once 'settings.php';
+
+	add_filter("plugin_action_links_$this->plugin_basename", array($this, 'filter_plugin_action_links'));
+    }
+
+    public function filter_plugin_action_links($links) {
+	$settings_link = '<a href="' . admin_url() . 'options-general.php?page=adpushup_settings_page' . '">Settings</a>';
+	array_unshift($links, $settings_link);
+
+	return $links;
     }
 
 }
@@ -37,3 +49,4 @@ function AdPushup() {
 }
 
 add_action('plugins_loaded', 'AdPushup');
+add_filter("plugin_action_links_$plugin", 'your_plugin_settings_link');
