7197b59020c5b1d2a2a1593b5484e3b8086db831
AdPushup <AdPushup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-10 20:10:03 +0000

    Release v1.3
    git-svn-id: https://plugins.svn.wordpress.org/adpushup/tags/1.3@1157459 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adpushup.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+  Plugin Name: AdPushup
+  Plugin URI: http://adpushup.com
+  Description: Maximize your AdSense Ad Revenue!
+  Version: 1.3
+  Author: AdPushup
+  Author URI: http://www.adpushup.com
+  License: GPL2
+  Updated: 02-04-2015
+ */
+
+// Prevent direct access to file
+defined('ABSPATH') or die("Cheatin' uh?");
+
+class AdPushup {
+
+    public $plugin_dir;
+    public $version = '1.3';
+    public $update_url;
+    public $platform;
+    public $plugin_basename;
+
+    public function __construct() {
+
+	$this->plugin_dir = plugin_dir_path(__FILE__);
+	$this->plugin_basename = plugin_basename(__FILE__);
+
+	require_once 'misc.php';
+	require_once 'injector.php';
+	require_once 'settings.php';
+
+	add_filter("plugin_action_links_$this->plugin_basename", array($this, 'filter_plugin_action_links'));
+    }
+
+    public function filter_plugin_action_links($links) {
+	$settings_link = '<a href="' . admin_url() . 'options-general.php?page=adpushup_settings_page' . '">Settings</a>';
+	array_unshift($links, $settings_link);
+
+	return $links;
+    }
+
+}
+
+function AdPushup() {
+    global $AdPushup;
+    $AdPushup = new AdPushup();
+}
+
+add_action('plugins_loaded', 'AdPushup');
