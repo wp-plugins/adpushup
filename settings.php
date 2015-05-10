43e95e2e7aea915bde7c8b320cb24024312b7c6f
AdPushup <AdPushup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-10 19:19:02 +0000

    Released v1.2
    git-svn-id: https://plugins.svn.wordpress.org/adpushup/tags/1.2@1157445 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ settings.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ *  Injects content into page at different places
+ */
+
+// Prevent direct access to file
+defined('ABSPATH') or die("Cheatin' uh?");
+
+class AdPushup_Settings {
+
+    function __construct() {
+	add_action('admin_menu', function () {
+	    add_options_page(
+		    'AdPushUp Settings', 'AdPushUp Settings', 'manage_options', 'adpushup_settings_page', 'adpushup_settings_page'
+	    );
+	});
+	add_action('admin_notices', array($this, 'action_admin_notices'));
+    }
+
+    function action_admin_notices() {
+	global $pagenow;
+
+	$adpush_code = get_option('adpushup_site_id', '');
+	$url = admin_url() . 'options-general.php?page=adpushup_settings_page&auto_close=yes';
+
+	// Show error message everywhere except its setting page
+	if (
+		!('options-general.php' == $pagenow && isset($_GET['page']) && 'adpushup_settings_page' == $_GET['page']) &&
+		empty($adpush_code)
+	) {
+	    echo "<script type='text/javascript'>
+                    // Popup window code
+                    function newPopup(url) {
+                        popupWindow = window.open(
+                            url,'popUpWindow','height=350,width=500,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=yes'
+                        )
+
+                    }
+		    function AdPushupSettingResult(result) {
+			if ('success' == result) {
+			    jQuery('#adpushup_error_site_id')[0].outerHTML = \"<div class='updated'><p>AdPushup Code updated successfully</p></div>\";
+			}
+		    }
+                </script>
+                
+		<div class='error' id='adpushup_error_site_id'><p>Please <b><a href='JavaScript:newPopup(\"$url\");'>install AdPushup site ID</a><b> to use it.</p></div>
+	    ";
+	}
+    }
+
+}
+
+new AdPushup_Settings();
+
+function adpushup_settings_page() {
+    $auto_close = isset($_REQUEST['auto_close']) && 'yes' == $_REQUEST['auto_close'];
+    if (isset($_REQUEST['adpushup_site_id'])) {
+	$adpushup_site_id = abs($_REQUEST['adpushup_site_id']);
+
+	if (isset($_POST['_wpnonce']) && wp_verify_nonce($_POST['_wpnonce'], '_adpushup_site_id')) {
+	    update_option('adpushup_site_id', $adpushup_site_id);
+	    echo "<br/><div class='updated'><p>Code updated successfully</p></div>";
+	    if ($auto_close) {
+		echo "<script type='text/javascript'>window.opener.AdPushupSettingResult('success');window.close();</script>";
+	    }
+	}
+    } else {
+	$adpushup_site_id = get_option('adpushup_site_id', '');
+    }
+    ?>
+          <h3>Please enter AdPushup code</h3>
+          <form method="POST" name="adpushup_submission_form">
+    <?php wp_nonce_field('_adpushup_site_id') ?>
+    	  <input type="number" name="adpushup_site_id" required size="50" value="<?php echo esc_attr($adpushup_site_id) ?>" />
+    <?php submit_button('Update code'); ?>
+          </form>
+    <?php
+}
