3dc29e20cc87b822fb77c54a1d65e65bae44a30b
AdPushup <AdPushup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-11 13:50:09 +0000

    dasdsa
    git-svn-id: https://plugins.svn.wordpress.org/adpushup/tags/0.8@1178981 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ injector.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ *  Injects content into page at different places
+ */
+
+// Prevent direct access to file
+defined('ABSPATH') or die("Cheatin' uh?");
+
+class AdPushup_Injector {
+
+    function __construct() {
+        add_action('wp_head', array($this, 'action_wp_head'), 1);
+        add_filter('the_content', array($this, 'filter_the_content'));
+    }
+
+    public function filter_the_content($content) {
+
+        $pre = '<div id="_ap_wp_content_start" style="display:none"></div>';
+        $post = '<div id="_ap_wp_content_end" style="display:none"></div>';
+        return $pre . $content . $post;
+    }
+
+    /**
+     * Runs at the end of <head> tag
+     */
+    public function action_wp_head() {
+        global $wp;
+        global $AdPushup;
+
+        $adpushup_site_id = get_option('adpushup_site_id', '');
+
+        $URI = home_url(add_query_arg(array(), $wp->request));
+        $site_domain = cleanUrl(get_site_url());
+        $plugin_version = $AdPushup->version;
+        $page_group = ucwords(getPageType());
+        $referer = '';
+
+        if (!empty($_SERVER['HTTP_REFERER'])) {
+            $ref_scheme = parse_url($_SERVER['HTTP_REFERER']);
+            $site_scheme = parse_url(get_site_url());
+            if ($ref_scheme['host'] != $site_scheme['host']) {
+                $referer = $ref_scheme['host'];
+            }
+        }
+
+        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+            $no_script_code = json_encode(array("success" => true, 'siteUrl' => cleanUrl(get_site_url()), "urls" => getUrlsForExperiment()), JSON_UNESCAPED_SLASHES);
+        } else {
+            $no_script_code = str_replace("\\/", "/", json_encode(array("success" => true, 'siteUrl' => cleanUrl(get_site_url()), "urls" => getUrlsForExperiment())));
+        }
+
+
+//                      AdPushup noscript code starts (Required for call by server)
+        echo '
+<noscript>
+    _ap_ufes' . $no_script_code . '_ap_ufee
+</noscript>
+';
+//                      AdPushup noscript code ends
+//                      AdPushup script code starts
+        echo '
+<script data-cfasync="false" type="text/javascript">
+(function () {
+	var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "//optimize.adpushup.com/' . esc_js($adpushup_site_id ? $adpushup_site_id . '/' : '') . 'ap.js";
+    (document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(s);
+
+    ((window.adpushup = window.adpushup || {}).configure = (window.adpushup.configure || [])).push({
+	    "siteId": "' . esc_js($adpushup_site_id) . '",
+	    "pageGroup": "' . $page_group . '",
+	    "siteDomain": "' . $site_domain . '",
+	    "pageUrl": "' . esc_js($URI) . '",
+	    "referrer": "' . esc_js($referer) . '",
+	    "cms": "wordpress",
+	    "pluginVer": "' . $plugin_version . '"
+    });
+})();
+</script>
+';
+//                      AdPushup script code ends
+    }
+
+}
+
+new AdPushup_Injector();
